9e10cc67b02f35b57736b404ced9412d9789eea5
Jxtopher <39927513+Jxtopher@users.noreply.github.com>
2021-08-21 17:27:05 +0200

    Update install-srv.sh

--- install/install-srv.sh
+++ install/install-srv.sh
@@ -1,5 +1,10 @@
+# Debian 
 
 URI="https://raw.githubusercontent.com/Jxtopher/config/main"
 
+# screen
 sudo apt-get install screen
 wget $(URI)/.screenrc -O /etc/screenrc
+
+# Privilage
+apt-get install sudo
