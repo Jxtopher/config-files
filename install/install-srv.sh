f8381bd0b0181c82336e42e5730c702d29f51915
Jxtopher <39927513+Jxtopher@users.noreply.github.com>
2021-08-22 20:01:54 +0200

    Update install-srv.sh

--- install/install-srv.sh
+++ install/install-srv.sh
@@ -1,7 +1,13 @@
 # VAR
 URI="https://raw.githubusercontent.com/Jxtopher/config/main"
-USER="chris"
-RSA_KEY_PUB="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCm0LL0DztzfyfaFG94cBnAmjXAljrRpIcU0Kr5vv/Jw51O9siIzWkwv+LxgCjzB95Rzx7BcoOYnP/Yh1RRRwvmcys45KpXwwRg3qevYtsZ9JTys+qt2oWwECfvo5FaS0x09mLn3NBClatJotQR81SnKOxq30K8iJFnA/XIX8RTGURfTFawVAQV3wWN3mo3yyJDm9TWlBeQqczVEwDFcNBHLZMObFwP2cv0CvKJAt51ODPF6EMcqkXRdqcJ/BuzWgRpdxfuNX9GTwBEWAKtdgnevh4BWoPKjH9w/s8wmX/6nYACK4ReaazxFIqCk3XCTvpsAA73tLlcBh0zCcQ/4Bf9AqTiNp17tIkxJADWl5tvrXCdNa/rHheK5vIsfrD1pmmEdlzYxtjwT85JFjZomMHnwcGcm8YMPCMwDTES4eym0erwTOwf3j3/b/bjgKj2kd3ggtaRH7R3kX5rck5X92xZfkQmz4I+vDeu8j1CvN+o5MortlnPLLS5L0DY0Vdn+NXMylpcpwEl4A0RaO3a6UcxbIZF8QmmDOl7dpMIxKC104bt9wXmwnJ2jHWXGeMGRAWlBj0BLGOQFYBKFhpKwjbSrK4RqhOeCLMpYNy8e3K5AgITPPWh0VbguOBb6FWVu0j6Pc00KrVf89MT0abxwxMiQvskuztNQvRg5rhEt6AL9w== chris@Zeus"
+USER=""
+RSA_KEY_PUB=""
+
+# Check root
+if [ $(whoami) != "root" ] ; then
+  >&2 echo "[-] Need to be root"
+  exit 1
+fi
 
 # OS Check
 source /etc/os-release
@@ -15,16 +21,16 @@ if [ "$USER" = "" ] || ! grep -q "$USER:" /etc/passwd ; then
   exit 1
 fi
 
-# # Install screen
-# sudo apt-get install -y screen
-# wget $(URI)/.screenrc -O /etc/screenrc
+# Install screen
+sudo apt-get install -y screen
+wget $(URI)/.screenrc -O /etc/screenrc
 
-# # vim
-# apt-get install -y vim
-# wget $(URI)/vimrc -O /etc/vimrc
+# vim
+apt-get install -y vim
+wget $(URI)/vimrc -O /etc/vimrc
 
 # Privilage
-# apt-get install -y sudo
+apt-get install -y sudo
 PATH_AUTHORIZED_KEYS="/home/$USER/.ssh/authorized_keys"
 if [ ! -f $PATH_AUTHORIZED_KEYS ]; then
     mkdir -p "/home/$USER/.ssh/" && touch $PATH_AUTHORIZED_KEYS
@@ -33,12 +39,18 @@ fi
 if ! grep -q "$RSA_KEY_PUB" $PATH_AUTHORIZED_KEYS ; then
   echo $RSA_KEY_PUB > /home/chris/.ssh/authorized_keys
 fi
+chmod 660 /etc/sudoers
+echo "$USER   ALL=(ALL:ALL) ALL" > /etc/sudoers
+chmod 440 /etc/sudoers
 
-# # sshd
-# apt-get install -y openssh-server
-# wget $(URI)/sshd_config -O /etc/sshd_config
-# systemctl restart sshd
+# sshd
+apt-get install -y openssh-server
+wget $(URI)/sshd_config -O /etc/sshd_config
+systemctl restart sshd
 
-# # nftables
-# wget $(URI)/nftables.conf -O /etc/nftables.conf
-# systemctl restart nftables
+# nftables
+wget $(URI)/nftables.conf -O /etc/nftables.conf
+systemctl restart nftables
+
+# Information
+echo "[+] Test the configuration and connect by ssh"
